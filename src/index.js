0d20912d9fa0a2d64cd14faf4a4ec3075cca8be3
zenliarth <54330804+zenliarth@users.noreply.github.com>
2022-01-14 13:07:14 -0300

    add backend

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db_products',
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  let sql = 'SELECT * FROM products';
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.post('/products', (req, res) => {
+  let data = req.body;
+  let sql = `INSERT INTO products (name, price, quantity, description ) VALUES ('${data.name}', '${data.price}', '${data.quantity}', '${data.description}')`;
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/products', (req, res) => {
+  let sql = 'SELECT * FROM products';
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.get('/products/:id', (req, res) => {
+  let sql = `SELECT * FROM products WHERE id = ${req.params.id}`;
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`🚀 Server is running on port 4000`);
+});
